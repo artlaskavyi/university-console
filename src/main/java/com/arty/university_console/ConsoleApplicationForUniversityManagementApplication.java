a853ae7018b78b008f981712f51d7cd7b4a02a6c
artlaskavyi <art.laskavyi@gmail.com>
2025-05-11 14:33:04 +0300

    Initial commit

---
+++ src/main/java/com/arty/university_console/ConsoleApplicationForUniversityManagementApplication.java
@@ -0,0 +1,13 @@
+package com.arty.university_console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleApplicationForUniversityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsoleApplicationForUniversityManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arty/university_console/ConsoleApplicationForUniversityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arty.university_console;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoleApplicationForUniversityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
